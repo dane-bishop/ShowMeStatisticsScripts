c968f9f24b1148aaa7baa3cc19142c8a3ab5b070
Melanie Bishop <MelanieBishop@MacBookPro.localdomain>
2025-10-15 09:31:32 -0500

    Quick update to test pulling

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ conn = get_db_connection()
 tsid = ensure_team_season(conn, year=2025)
 upsert_roster(conn, tsid, get_roster('baseball', 2025))
 upsert_schedule(conn, tsid, get_schedule_text('baseball', 2025))
+
+#
