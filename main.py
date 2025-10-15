2ba7542419899f52cc589a88e4151a8afa906535
Melanie Bishop <MelanieBishop@MacBookPro.localdomain>
2025-10-15 10:33:24 -0500

    Made function callable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py (sketch)
-from roster import get_roster
+from roster.get_roster import get_roster
 from schedule.upsert_schedule import upsert_schedule
-from schedule import get_schedule_html
+from schedule.get_schedule_html import get_schedule_html
 from roster.upsert_roster import upsert_roster
 from helpers.core import get_db_connection
 from schedule.ensure_team_season import ensure_team_season
@@ -10,8 +10,8 @@ conn = get_db_connection()
 tsid = ensure_team_season(conn)
 
 # tsid already exists (team_season 2025)
-people = get_roster('baseball', 2025)
+people = list(get_roster('baseball', 2025))
 upsert_roster(conn, tsid, people)
 
-games = get_schedule_html('baseball', 2025, debug=True)
+games = list(get_schedule_html('baseball', 2025, debug=True))
 upsert_schedule(conn, tsid, games)
