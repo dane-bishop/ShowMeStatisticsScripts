b75f90d3605e17e22f77cf88e63503b345583a26
Dane Bishop <dbbzk7@umsystem.edu>
2025-10-26 20:42:00 +0000

    Added pitching stats scraper to main

--- main.py
+++ main.py
@@ -6,28 +6,31 @@ from schedule.schedule_helpers.ensure_team_season import ensure_team_season
 from requests import Session 
 from stats.parse_player_hitting import get_player_hitting_mu
 from stats.upsert_player_hitting import upsert_player_batting_gamelog, upsert_player_hitting_season_highs
+from stats.parse_player_pitching import get_player_pitching_mu
+from stats.upsert_player_pitching import upsert_player_pitching_gamelog, upsert_player_pitching_season_highs
 from helpers.core import BASE
 
 conn = get_db_connection()
 tsid = ensure_team_season(conn, school="Missouri", sport_key="baseball", sport_name="Baseball", year=2025, sport_slug="baseball")
 
 
-# Stream roster straight into DB
-
-
 YEARS = {
     2025
 }
 
-'''
+
+
+
 # GET BASEBALL ROSTER BY SEASON
+'''
 for year in YEARS:
     tsid = ensure_team_season(conn, school="Missouri", sport_key="baseball", sport_name="Baseball", year=year, sport_slug="baseball")
     for person in get_roster_baseball('baseball', year):
         upsert_roster_baseball(conn, tsid, [person]) 
-
+'''
 
 # GET BASEBALL SCHEDULE BY SEASON
+'''
 for game in get_schedule_baseball('baseball', 2025, debug=False):
     upsert_schedule_baseball(conn, tsid, [game])
 
@@ -35,7 +38,9 @@ for game in get_schedule_baseball('baseball', 2025, debug=False):
 
 
 
-# GET BASEBALL PLAYER STATS BY PLAYER LINK
+# GET BASEBALL PLAYER HITTING STATS BY PLAYER LINK
+
+'''
 sess = Session()
 #player_id = 4  
 #roster_player_id = 26046      
@@ -57,6 +62,7 @@ with conn.cursor() as cur:
 print(f"Adding data for {len(batters)} batters")
 
 
+
 for (player_id, roster_player_id) in batters:
     print(f"Player ID: {player_id} - Roster Player ID: {roster_player_id}")
 
@@ -66,3 +72,30 @@ for (player_id, roster_player_id) in batters:
 
     upsert_player_batting_gamelog(conn, player_id=player_id, rows=parsed["gamelog"])
     upsert_player_hitting_season_highs(conn, player_id=player_id, highs=parsed["season_highs"])
+'''
+
+
+# GET BASEBALL PLAYER PITCHING STATS BY PLAYER LINK
+sess = Session()
+year = 2025
+
+with conn.cursor() as cur:
+    cur.execute("""
+    SELECT DISTINCT p.id, p.player_id
+    FROM players p
+    JOIN roster_memberships rm
+    ON rm.player_id = p.id
+    WHERE rm.position IS NOT NULL AND UPPER(rm.position) LIKE ANY (ARRAY['%RHP%', '%LHP%])
+    """)
+    pitchers = cur.fetchall()
+
+print(f"Adding data for {len(pitchers)} pitchers")
+
+for (player_id, roster_player_id) in pitchers:
+    print(f"Player ID: {player_id} - Roster Player ID: {roster_player_id}")
+
+    parsed = get_player_pitching_mu(sess, roster_player_id, year)
+    print("first 2 rows: ", parsed["gamelog"][:2])
+
+    upsert_player_pitching_gamelog(conn, player_id=player_id, rows=parsed["gamelog"])
+    upsert_player_pitching_season_highs(conn, player_id=player_id, highs=parsed["season_highs"])
